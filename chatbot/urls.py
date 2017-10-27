d1dfb403917ccc2aeb6e8fb2e45a635a71b90339
Gurpreet Singh <gurpreetsingh00885@gmail.com>
2017-10-27 19:32:31 +0530

    Add ChatView to chatbot.views
    The chatview at present uses the Watson API to extract the most
    relevant topic from the text. Further functionality to be implemented.

---
+++ chatbot/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from.views import ChatView
+
+urlpatterns = [
+    url(r'^',  ChatView.as_view()),
+]
--- chatbot/views.py
+++ chatbot/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django import views
+from django.http import JsonResponse, HttpResponse
+import requests
 
-# Create your views here.
+class ChatView(views.View):
+	def get(self, request, *args, **kwargs):
+		text = request.GET['text']
+		url = "https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2017-02-27&text=%s&features=concepts" %(text)
+		r = requests.get(url=url, auth=requests.auth.HTTPBasicAuth('55ea49b2-c353-463a-8d25-b3c358fa1df3', 'z5AU0l7jtd3c')).json()
+		if "error" in r:
+			return JsonResponse(r)
+		concept = 'general'
+		try:
+			concept = r['concepts'][0]['text']
+		except:
+			pass
+		print(concept)
+		return JsonResponse(r)
